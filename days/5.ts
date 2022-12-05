224ee44bd3ba2a92f14c84bf2b19abc18c93a0ad
Will Hack <will.j.hack@gmail.com>
2022-12-05 12:40:21 -0500

    abstract funtionality

--- days/5.ts
+++ days/5.ts
@@ -2,10 +2,23 @@ interface Stacks {
   [k: number]: string[];
 }
 
+function processInput(rawInput: string) {
+  /* Also fixes misaligned puzzle input */
+  const [initialMap, rawMoves] = `${' '.repeat(8) + rawInput}`.split('\n\n');
+  const stacks = buildStacks(initialMap);
+
+  const moves = rawMoves.split('\n').map((rm) => {
+    const [_move, m, _from, f, _to, t] = rm.split(' ');
+    return { move: +m, from: +f - 1, to: +t - 1 };
+  });
+
+  return { stacks, moves };
+}
+
 function buildStacks(initMap: string) {
   const res: Stacks = {};
   const stackRows = initMap.split('\n');
-  const stackDepth = stackRows?.pop()?.at(-2) || 0;
+  const stackDepth = stackRows.pop()?.at(-2) || 0;
 
   const parsedStack = stackRows.map((r: string) => {
     const parsed = [];
@@ -26,42 +39,30 @@ function buildStacks(initMap: string) {
   return res;
 }
 
-export function partOne(rawInput: string) {
-  //---- Start: fix broken puzzle input
-  const t = ' '.repeat(8) + rawInput;
-  //---- End: fix broken puzzle input
-
-  const [init, moves] = t.split('\n\n');
-
-  const stacks = buildStacks(init);
-  moves.split('\n').forEach((m: string) => {
-    const [_move, move, _from, from, _to, to] = m.split(' ');
-    for (let i = 0; i < +move; i++) {
-      stacks[+to - 1].push(stacks[+from - 1].pop() ?? '');
-    }
-  });
-
-  const res = Object.values(stacks)
+const exportTopContainer = (stacks: Stacks) =>
+  Object.values(stacks)
     .map((arr) => arr[arr.length - 1])
     .join('');
-  return res;
+
+export function partOne(rawInput: string) {
+  const { stacks, moves } = processInput(rawInput);
+
+  for (const { move, from, to } of moves) {
+    for (let i = 0; i < move; i++) {
+      const container = stacks[from].pop();
+      if (container === undefined) throw new Error('Someone stole our container!');
+      stacks[to].push(container);
+    }
+  }
+  return exportTopContainer(stacks);
 }
 
 export function partTwo(rawInput: string) {
-  //---- Start: fix broken puzzle input
-  const t = ' '.repeat(8) + rawInput;
-  //---- End: fix broken puzzle input
+  const { stacks, moves } = processInput(rawInput);
 
-  const [init, moves] = t.split('\n\n');
-
-  const stacks = buildStacks(init);
-  moves.split('\n').forEach((m: string) => {
-    const [_move, move, _from, from, _to, to] = m.split(' ');
-    stacks[+to - 1].push(...stacks[+from - 1].splice(stacks[+from - 1].length - +move, +move));
-  });
-
-  const res = Object.values(stacks)
-    .map((arr) => arr[arr.length - 1])
-    .join('');
-  return res;
+  for (const { move, from, to } of moves) {
+    const containers = stacks[from].splice(stacks[from].length - move, move);
+    stacks[to].push(...containers);
+  }
+  return exportTopContainer(stacks);
 }
